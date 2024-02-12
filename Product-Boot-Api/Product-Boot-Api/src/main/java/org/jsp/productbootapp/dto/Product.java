e79498780e1b15d6918840652c3a38de8ffba46b
Lavkushr06 <157868332+Lavkushr06@users.noreply.github.com>
2024-02-12 14:47:06 +0530

    Add files via upload

---
+++ Product-Boot-Api/Product-Boot-Api/src/main/java/org/jsp/productbootapp/dto/Product.java
@@ -0,0 +1,63 @@
+package org.jsp.productbootapp.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pid;
+	@Column(nullable = false)
+	private String pname;
+	@Column(nullable = false)
+	private String description;
+	@Column(nullable = false)
+	private double price;
+	@Column(nullable = false)
+	private int quantity;
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
